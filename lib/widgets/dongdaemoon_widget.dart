9579dcf7b41c4f3a1f77198787051999ffb0c64a
Chris Ha <chunghha@users.noreply.github.com>
2020-05-28 14:37:19 -0500

    refactor: apply ConstrainedBox to the widgets

--- lib/widgets/dongdaemoon_widget.dart
+++ lib/widgets/dongdaemoon_widget.dart
@@ -11,37 +11,47 @@ class DongdaemmonWidget extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(9.0, 7.0, 9.0, 0.0),
         child: Container(
             color: '#ebeffa'.toColor(),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CachedNetworkImage(
-                    imageUrl:
-                        'https://st3.depositphotos.com/6193152/15484/v/600/depositphotos_154841576-stock-video-4k-timelapse-of-the-dongdaemun.jpg',
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-                  SizedBox(height: 4.0),
-                  Text('Dongdaemoon Plaza',
-                      style: GoogleFonts.poppins(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.w500,
-                          textStyle: TextStyle(color: '#2e3440'.toColor()))),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Container(
-                      padding: EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 2.0),
-                      child: Text(
-                          '  The Dongdaemun Design Plaza, also called the DDP, is a major urban development landmark in Seoul, ' +
-                              'South Korea designed by Zaha Hadid and Samoo, with a distinctively neofuturistic design characterized ' +
-                              'by the powerful, curving forms of elongated structures.',
-                          style: GoogleFonts.openSans(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w400,
-                              textStyle: TextStyle(
-                                  color: '#2e3440'.toColor(),
-                                  letterSpacing: 1.2)))),
-                ])));
+            child: SingleChildScrollView(
+                child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          CachedNetworkImage(
+                            imageUrl:
+                                'https://st3.depositphotos.com/6193152/15484/v/600/depositphotos_154841576-stock-video-4k-timelapse-of-the-dongdaemun.jpg',
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                          SizedBox(height: 4.0),
+                          Text('Dongdaemoon Plaza',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.w500,
+                                  textStyle:
+                                      TextStyle(color: '#2e3440'.toColor()))),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Container(
+                              padding: EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 2.0),
+                              child: Text(
+                                  '  The Dongdaemun Design Plaza, also called the DDP, is a major urban development landmark in Seoul, ' +
+                                      'South Korea designed by Zaha Hadid and Samoo, with a distinctively neofuturistic design characterized ' +
+                                      'by the powerful, curving forms of elongated structures.',
+                                  style: GoogleFonts.openSans(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w400,
+                                      textStyle: TextStyle(
+                                          color: '#2e3440'.toColor(),
+                                          letterSpacing: 1.2)))),
+                          SizedBox(
+                            height:
+                                36.0, // just to scroll with this SingleChildScrollView
+                          ),
+                        ])))));
   }
 }
--- lib/widgets/lotte_widget.dart
+++ lib/widgets/lotte_widget.dart
@@ -11,37 +11,47 @@ class LotteWidget extends StatelessWidget {
         padding: EdgeInsets.fromLTRB(9.0, 7.0, 9.0, 0.0),
         child: Container(
             color: '#ebeffa'.toColor(),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CachedNetworkImage(
-                    imageUrl:
-                        'https://www.globalcovenantofmayors.org/wp-content/uploads/2019/02/lotte-world-tower-1791802_1280.jpg',
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                  ),
-                  SizedBox(height: 4.0),
-                  Text('Lotte Tower',
-                      style: GoogleFonts.poppins(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.w500,
-                          textStyle: TextStyle(color: '#2e3440'.toColor()))),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Container(
-                      padding: EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 2.0),
-                      child: Text(
-                          '  Lotte World Tower is a 123-floor, 555-metre supertall skyscraper located in Seoul, ' +
-                              'South Korea. It opened to the public on April 11, 2017 and is currently the tallest ' +
-                              'building in South Korea, and is the 6th tallest building in the world.',
-                          style: GoogleFonts.openSans(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w400,
-                              textStyle: TextStyle(
-                                  color: '#2e3440'.toColor(),
-                                  letterSpacing: 1.2)))),
-                ])));
+            child: SingleChildScrollView(
+                child: new ConstrainedBox(
+                    constraints: new BoxConstraints(),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          CachedNetworkImage(
+                            imageUrl:
+                                'https://www.globalcovenantofmayors.org/wp-content/uploads/2019/02/lotte-world-tower-1791802_1280.jpg',
+                            placeholder: (context, url) =>
+                                CircularProgressIndicator(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                          SizedBox(height: 4.0),
+                          Text('Lotte Tower',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.w500,
+                                  textStyle:
+                                      TextStyle(color: '#2e3440'.toColor()))),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Container(
+                              padding: EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 2.0),
+                              child: Text(
+                                  '  Lotte World Tower is a 123-floor, 555-metre supertall skyscraper located in Seoul, ' +
+                                      'South Korea. It opened to the public on April 11, 2017 and is currently the tallest ' +
+                                      'building in South Korea, and is the 6th tallest building in the world.',
+                                  style: GoogleFonts.openSans(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w400,
+                                      textStyle: TextStyle(
+                                          color: '#2e3440'.toColor(),
+                                          letterSpacing: 1.2)))),
+                          SizedBox(
+                            height:
+                                36.0, // just to scroll with this SingleChildScrollView
+                          ),
+                        ])))));
   }
 }
